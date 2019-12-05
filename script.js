8d76b09136f72557aca020c4ee6cf37e90b817b2
Christopher Melby <melby.christopher@gmail.com>
2019-12-05 09:58:37 -0800

    figuring out how to structure my js, pulled in helpfull examples from the activities.

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
 
 
 //----use this as example for hiding the start btn and starting the next div------//
-var eventType = document.querySelector("#event-type"); 
+var startbtnEl = document.querySelector("#start-btn"); 
 var mouseEventsEl = document.querySelector("#click-events");
 var keyEventsEl = document.querySelector("#key-events");
 
@@ -24,3 +24,17 @@ function toggleDisplay(event) {
     keyEventsEl.classList.add("hide");
   }
 }
+
+
+//-----use this function as an example for dynamically moving through the question and answers--//
+function addPersonToList(event) { //creat my own function 
+  event.preventDefault();
+  var name = nameEl.value;
+  var li = document.createElement("li");//target my list
+  li.id = people.length;
+  li.innerHTML = name + " <button>edit</button>";//make this the [] of possible answers
+  people.push({ name: name });
+  peopleListEl.append(li);
+}
+//I am going to need a for loop to grab my + questions[questionindex] choices[i],
+// questions++
